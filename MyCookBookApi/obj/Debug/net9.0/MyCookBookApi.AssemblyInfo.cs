dfdbfe43d045f7d1bd59c2cd11e5dc703074b1b5
Kenyang <kenyanglual05@gmail.com>
2025-01-19 21:19:31 -0500

    Assignment 2 Task 4 Completion

--- MyCookBookApi/obj/Debug/net9.0/MyCookBookApi.AssemblyInfo.cs
+++ MyCookBookApi/obj/Debug/net9.0/MyCookBookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCookBookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da7e100ed96c125a509d61ebd27f0b450af34494")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72478007ede25a866e650b878746b27e4174440d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCookBookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCookBookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MyCookBookApp/Services/RecipeService.cs
+++ MyCookBookApp/Services/RecipeService.cs
@@ -17,16 +17,14 @@ namespace MyCookBookApp.Services
         public async Task<List<Recipe>> GetRecipesAsync()
         {
             // Perform the HTTP GET request
-            var response = await _httpClient.GetAsync("http://localhost:5165/api/recipe");
+            var response = await _httpClient.GetAsync("http://localhost:5090/api/recipe");
 
-            // Ensure the response was successful (throws exception for non-success codes)
             response.EnsureSuccessStatusCode();
 
             // Read and deserialize the JSON response
             var json = await response.Content.ReadAsStringAsync();
             var recipes = JsonConvert.DeserializeObject<List<Recipe>>(json);
 
-            // Return the deserialized recipes or an empty list if null
             return recipes ?? new List<Recipe>();
         }
     }
