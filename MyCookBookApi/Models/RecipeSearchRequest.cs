48349b3715202ab9759e2c72feefdaeec8b231ac
Kenyang <kenyanglual05@gmail.com>
2025-02-23 19:57:39 -0500

    Assignment 4 Step 1.3: Update the Search Request Model

--- MyCookBookApi/Models/RecipeSearchRequest.cs
+++ MyCookBookApi/Models/RecipeSearchRequest.cs
@@ -1,4 +1,8 @@
-public class RecipeSearchRequest
+namespace MyCookBookApi.Models
 {
-    public string? Query { get; set; }
+    public class RecipeSearchRequest
+    {
+        public string Keyword { get; set; }
+        public List<CategoryType> Categories { get; set; } = new List<CategoryType>();
+    }
 }
